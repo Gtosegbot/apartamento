a02faeb7b08cec47f55843796c1bd230be55f2a6
Gtosegbot <94094737+Gtosegbot@users.noreply.github.com>
2025-04-28 18:59:16 -0300

    Update analytics.js
    cria analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,13 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XXXXXXXXXX');
+gtag('config', 'AW-3179137944'); // Configuração para Google Ads
 
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-XXXXXXX');
